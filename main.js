1cfc29d8c15313d479ca2e9ee2e86725ac985095
Your Namesergio-jc <sergiojara0609@gmail.com>
2022-08-23 23:14:59 -0500

    add dinamic height bar

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ import data from "./data.json" assert { type: "json" };
 
 let chartBarsContainer = document.querySelector(".chart__bars-container");
 
+let values = []
+
 data.forEach((element) => {
+    values.push(element.amount)
   chartBarsContainer.innerHTML += `
   <div class="chart__bar">
     <div class="chart__bar--label">$${element.amount}</div>
@@ -10,10 +13,31 @@ data.forEach((element) => {
   </div>`;
 });
 
+let alturaMaxBarpx = 150;
+let maxValue = Math.max(...values);
+
+    /*
+    52.36 -> 200
+    17.45 -> x
+    x = (17.45 * 200px) / 52.36
+
+    alturaActualpx = ( nuevoValor *  alturaMaxBarpx) / maxValor
+    */
+
 let bars = document.querySelectorAll(".chart__bar");
 bars = [...bars];
 
 bars.forEach((bar) => {
+
+    let newValue =  parseFloat(bar.childNodes[1].innerText.slice(1));
+    let alturaActualpx  = (newValue*alturaMaxBarpx) / maxValue
+    bar.style.height = `${alturaActualpx}px`
+
+    // pintar la barra con mas valor 
+    if(newValue == maxValue) bar.style.backgroundColor ="hsl(186,34%,60%)"
+
+
+
     bar.addEventListener("mouseover", (event)=> {
         let labelElement = event.target.childNodes[1]; 
         labelElement.style.display = "block"
